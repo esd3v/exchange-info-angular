1e35b14683de3511d314f9033edf75de80a06365
esd3v <57869510+esd3v@users.noreply.github.com>
2022-07-21 03:36:39 +0700

    Add missing modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppStoreModule } from './store/store.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './components/app/app.component';
 import { HomeComponent } from './components/home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { InterceptorService } from './services/interceptor.service';
 import { HeaderComponent } from './components/header/header.component';
+import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
 import { SkeletonComponent } from './components/skeleton/skeleton.component';
 
 @NgModule({
@@ -19,6 +20,7 @@ import { SkeletonComponent } from './components/skeleton/skeleton.component';
     BrowserAnimationsModule,
     AppStoreModule,
     HttpClientModule,
+    MatToolbarModule,
     NgxSkeletonLoaderModule,
   ],
   providers: [
