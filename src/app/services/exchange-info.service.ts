a6831d1d2280e0efe004013f5e28f7ce8e2bb5e1
esd3v <57869510+esd3v@users.noreply.github.com>
2022-07-21 20:36:58 +0700

    Update services

--- src/app/services/exchange-info.service.ts
+++ src/app/services/exchange-info.service.ts
@@ -10,29 +10,31 @@ import { HttpClient } from '@angular/common/http';
 export class ExchangeInfoService {
   constructor(private http: HttpClient) {}
 
+  parseExchangeInfo(exchangeInfo: ExchangeInfo): ExchangeInfoParsed {
+    const symbols = exchangeInfo.symbols;
+    const firstSymbol = symbols && symbols[0];
+    const baseAsset = firstSymbol?.baseAsset;
+    const quoteAsset = firstSymbol?.quoteAsset;
+    const serverTime = exchangeInfo.serverTime;
+
+    const symbolInfo = symbols.map(({ baseAsset, quoteAsset, status }) => ({
+      symbol: `${baseAsset}${quoteAsset}`,
+      baseAsset,
+      quoteAsset,
+      status,
+    }));
+
+    return {
+      baseAsset,
+      quoteAsset,
+      serverTime,
+      symbolInfo,
+    };
+  }
+
   get(): Observable<ExchangeInfoParsed> {
-    return this.http.get<ExchangeInfo>('exchangeInfo').pipe(
-      map((data) => {
-        const symbols = data.symbols;
-        const firstSymbol = symbols && symbols[0];
-        const baseAsset = firstSymbol?.baseAsset;
-        const quoteAsset = firstSymbol?.quoteAsset;
-        const serverTime = data.serverTime;
-
-        const symbolInfo = symbols.map(({ baseAsset, quoteAsset, status }) => ({
-          symbol: `${baseAsset}${quoteAsset}`,
-          baseAsset,
-          quoteAsset,
-          status,
-        }));
-
-        return {
-          baseAsset,
-          quoteAsset,
-          serverTime,
-          symbolInfo,
-        };
-      })
-    );
+    return this.http
+      .get<ExchangeInfo>('exchangeInfo')
+      .pipe(map(this.parseExchangeInfo));
   }
 }
