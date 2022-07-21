1f03bc342fd05b5ada6b3787b38b6ee26cf94a80
esd3v <57869510+esd3v@users.noreply.github.com>
2022-07-21 20:45:58 +0700

    Set default pair dynamically if null on start

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,8 @@
-import { DEFAULT_PAIR } from './config';
 import { HomeComponent } from './components/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: '',
-    redirectTo: DEFAULT_PAIR,
-    pathMatch: 'full',
-  },
   {
     path: ':pair',
     component: HomeComponent,
--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -1,12 +1,13 @@
+import { ActivatedRoute, Router } from '@angular/router';
 import { TickerService } from './../../services/ticker.service';
-import { ExchangeInfoService } from './../../services/exchange-info.service';
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { Store } from '@ngrx/store';
-import { combineLatestWith } from 'rxjs';
+import { combineLatestWith, first, last, range, takeLast } from 'rxjs';
 import { SITE_NAME } from 'src/app/config';
 import { formatLastPrice } from 'src/app/helpers';
 import { actions, AppState, selectors } from 'src/app/store';
+import { ExchangeInfoService } from 'src/app/services/exchange-info.service';
 
 @Component({
   selector: 'app-root',
@@ -14,19 +15,27 @@ import { actions, AppState, selectors } from 'src/app/store';
 })
 export class AppComponent implements OnInit {
   constructor(
+    private route: ActivatedRoute,
+    private router: Router,
     private titleService: Title,
     private store: Store<AppState>,
-    private tickerService: TickerService
+    private tickerService: TickerService,
+    private exchangeInfoService: ExchangeInfoService
   ) {}
 
-  ngOnInit(): void {
-    const globalSymbol$ = this.store.select(selectors.global.globalSymbol);
-    const globalPair$ = this.store.select(selectors.global.globalPair);
-    const lastPrice$ = this.store.select(selectors.ticker.lastPrice);
+  globalSymbol$ = this.store.select(selectors.global.globalSymbol);
+  globalPair$ = this.store.select(selectors.global.globalPair);
+  lastPrice$ = this.store.select(selectors.ticker.lastPrice);
 
-    // Set title
-    globalPair$
-      .pipe(combineLatestWith(lastPrice$))
+  ngOnInit(): void {
+    this.setTitle();
+    this.createTicker();
+    this.handleEmptyPair();
+  }
+
+  setTitle() {
+    this.globalPair$
+      .pipe(combineLatestWith(this.lastPrice$))
       .subscribe(([globalPair, _lastPrice]) => {
         this.store.select(selectors.ticker.lastPrice).subscribe((data) => {
           const lastPrice = data;
@@ -42,12 +51,34 @@ export class AppComponent implements OnInit {
           this.titleService.setTitle(title);
         });
       });
+  }
 
-    globalSymbol$.subscribe((data) => {
+  createTicker() {
+    this.globalSymbol$.subscribe((data) => {
       if (data) {
         this.tickerService.get({ symbol: data }).subscribe((data) => {
           this.store.dispatch(actions.ticker.createTicker({ payload: data }));
         });
+      }
+    });
+  }
+
+  // If we opened root without pair param
+  handleEmptyPair() {
+    this.router.events.subscribe((data: unknown) => {
+      const { type } = data as Event;
+
+      // If navigation ended
+      if (Number(type) === 1) {
+        // If root
+        if (!this.route.children.length) {
+          this.exchangeInfoService
+            .get()
+            .subscribe(({ baseAsset, quoteAsset }) => {
+              const pair = `${baseAsset}_${quoteAsset}`;
+              this.router.navigate([pair]);
+            });
+        }
       }
     });
   }
