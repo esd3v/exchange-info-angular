6198e1f679e18666bf025778138441e12aa42674
esd3v <57869510+esd3v@users.noreply.github.com>
2023-01-24 04:44:14 +0700

    Update config

--- src/app/shared/config.ts
+++ src/app/shared/config.ts
@@ -1,7 +1,7 @@
 export const SITE_NAME = 'Binance Exchange Info';
 
 export const API_HTTP_VERSION = 3;
-export const API_HTTP_BASEURL = `https://api.binance.com/api/v${API_HTTP_VERSION}`;
+export const API_HTTP_BASEURL = `https://data.binance.com/api/v${API_HTTP_VERSION}`;
 export const API_HTTP_TIMEOUT = 30000;
 export const API_WEBSOCKET_BASEURL = 'wss://stream.binance.com:9443/ws';
 
