18a25dabe107eee753b09302e4bed4a7483f7c38
ES <esd3v@proton.me>
2023-07-18 00:26:21 +0700

    Update exchange-info facade

--- src/app/features/exchange-info/services/exchange-info-facade.service.ts
+++ src/app/features/exchange-info/services/exchange-info-facade.service.ts
@@ -1,28 +1,17 @@
 import { Injectable } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { filter, first, map } from 'rxjs';
 import { AppState } from 'src/app/store';
+import { symbolsSelectors } from '../../symbols/store';
 import { exchangeInfoActions, exchangeInfoSelectors } from '../store';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ExchangeInfoFacade {
-  public status$ = this.store$.select(exchangeInfoSelectors.status);
+  public restStatus$ = this.store$.select(exchangeInfoSelectors.status);
 
-  public successCurrent$ = this.status$.pipe(
-    first(),
-    filter((status) => status === 'success')
-  );
-
-  public successUntil$ = this.status$.pipe(
-    filter((status) => status === 'success'),
-    first()
-  );
-
-  public isLoading$ = this.status$.pipe(
-    map((status) => (status === 'init' ? null : status === 'loading'))
-  );
+  // TODO Move?
+  public tradingSymbols$ = this.store$.select(symbolsSelectors.tradingSymbols);
 
   public constructor(private store$: Store<AppState>) {}
 
