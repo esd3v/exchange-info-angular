72892b2aeeede2fc13864d36dfed6a5b8ecef973
ES <esd3v@proton.me>
2023-07-18 00:21:16 +0700

    Use new skeleton

--- src/app/features/order-book/components/order-book-table/order-book-table.module.ts
+++ src/app/features/order-book/components/order-book-table/order-book-table.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
-import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
 import { OrderBookTableComponent } from './order-book-table.component';
 import { WindowModule } from 'src/app/shared/components/window/window.module';
+import { SkeletonModule } from 'src/app/shared/components/skeleton/skeleton.module';
 
 @NgModule({
   declarations: [OrderBookTableComponent],
@@ -12,7 +12,7 @@ import { WindowModule } from 'src/app/shared/components/window/window.module';
     CommonModule,
     MatTableModule,
     MatPaginatorModule,
-    NgxSkeletonLoaderModule,
+    SkeletonModule,
     WindowModule,
   ],
   exports: [OrderBookTableComponent],
--- src/app/features/ticker/components/ticker-card/ticker-card.module.ts
+++ src/app/features/ticker/components/ticker-card/ticker-card.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TickerCardComponent } from './ticker-card.component';
 import { MatCardModule } from '@angular/material/card';
-import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
+import { SkeletonModule } from 'src/app/shared/components/skeleton/skeleton.module';
 
 @NgModule({
   declarations: [TickerCardComponent],
-  imports: [CommonModule, MatCardModule, NgxSkeletonLoaderModule],
+  imports: [CommonModule, MatCardModule, SkeletonModule],
   exports: [TickerCardComponent],
 })
 export class TickerCardModule {}
